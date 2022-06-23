267b6211b29542b86b1cea26c9c11c9301e6d9a1
luaroxy <luannamsiqueira@gmail.com>
2022-06-22 18:19:16 -0600

    webpack setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("It's working!")
+import './style.css';
+import Icon from './images/krustykrab.png';
+
+function component() {
+   const element = document.createElement('div');
+
+   element.textContent = ("Testing");
+   element.classList.add('hello');
+
+   return element;
+}
+
+document.body.appendChild(component());
