ab236ad2cfc828d35e229aabdd2c96aaeb17f836
luaroxy <luannamsiqueira@gmail.com>
2022-06-25 22:38:05 -0600

    Started menu page

---
+++ src/menu.js
@@ -0,0 +1,96 @@
+function page_load(){
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    function header() {
+      const header = document.createElement('div');
+      header.classList.add('header');
+
+      const titletop = document.createElement('div');
+      titletop.textContent = ("Krusty Krab");
+
+      const optionstop = document.createElement('div');
+      optionstop.classList.add('optionstop');
+      const hometop = document.createElement('div');
+      hometop.textContent = ("Home");
+      optionstop.appendChild(hometop);
+      const menutop = document.createElement('div');
+      menutop.textContent = ("Menu");
+      optionstop.appendChild(menutop);
+      const contacttop = document.createElement('div');
+      contacttop.textContent = ("Contact");
+      optionstop.appendChild(contacttop);
+
+      header.appendChild(titletop);
+      header.appendChild(optionstop);
+
+      return header;
+    }
+    
+    function menuContent(){
+        const menu = document.createElement('section');
+        menu.classList.add('menuContent');
+
+        const menuTitle = document.createElement('p');
+        menuTitle.textContent = ("Galley Grub");
+        menu.appendChild(menuTitle);
+        menuTitle.classList.add('menuTitle');
+
+        let menuItemPrice=["Krabby Patty","1.25","Krabby Meal","3.50",
+                            "   w/ sea cheese","1.50","Double Krabby Meal","3.75",
+                            "Double Krabby Patty","2.00","Triple Krabby Meal","4.00",
+                            "   w/ sea cheese","2.25","Salty Sea Dog","1.25",
+                            "Triple Krabby Patty","3.00","Footlong","2.00",
+                            "   w/ sea cheese","3.25","Sailors Surprise","3.00",
+                            " "," ","Golden Loaf","2.00",
+                            "Coral bits"," ","   w/ sauce","2.50",
+                            "   Small","1.00"," "," ",
+                            "   Medium","1.25","Kelp Shake","2.00",
+                            "   Large","1.75","Seafoam Soda"," ",
+                            " "," ","   Small","1.00",
+                            "Kelp Rings","1.50","   Medium","1.25",
+                            "   salty sauce","0.50","   Large","1.50"
+                          ];
+
+        menuItemPrice.forEach(element => {
+          const item = document.createElement('p');
+          item.textContent = element;
+          menu.appendChild(item);
+        });
+
+        return menu;
+    }
+
+    function footer(){
+        const footer = document.createElement('footer');
+        footer.classList.add('footer');
+     
+        const title = document.createElement('div');
+        title.textContent = ("Krusty Krab");
+     
+        const options = document.createElement('div');
+        options.classList.add('footeroptions');
+        const homebottom = document.createElement('div');
+        homebottom.textContent = ("Home");
+        options.appendChild(homebottom);
+        const menubottom = document.createElement('div');
+        menubottom.textContent = ("Menu");
+        options.appendChild(menubottom);
+        const contactbottom = document.createElement('div');
+        contactbottom.textContent = ("Contact");
+        options.appendChild(contactbottom);
+     
+        footer.appendChild(title);
+        footer.appendChild(options);
+     
+        return footer;
+     }
+
+    container.appendChild(header());
+    container.appendChild(menuContent());
+    container.appendChild(footer());
+
+    return container;
+}
+
+export default page_load;
